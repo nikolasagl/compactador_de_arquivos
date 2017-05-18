6e7b48e3a2bfc986451fe3371dd5c1a0f80ad72c
nikolasagl <nikolasagl@hotmail.com>
2017-05-18 14:10:27 -0300

    Scripts agora estão certos. Get_file_csv configurado para executar 50 vezes por arquivo Variavel compacted_file_size adicionada

--- scripts/get_file_csv.py
+++ scripts/get_file_csv.py
@@ -8,29 +8,29 @@ import csv
 #Definição da função que compacta o arquivo e gera csv com o time de execução da compactação
 def csv_creator(codigo1, codigo2, nome_arq, arquivo):
     #comando que será escrito no terminal
-    command = ['time', codigo1, codigo2, nome_arq, arquivo]
+    time_command = ['time', codigo1, codigo2, nome_arq, arquivo]
+    size_command = ['du', '-hsb', nome_arq]
 
     #Abre um 'terminal' e executa o comando passado por parametro
     #aqui stderr e stdout recebem um pipe vindo do subprocess popen. obs: nao sei pra que ainda
-    p = subprocess.Popen(command, stderr=subprocess.PIPE, stdout=subprocess.PIPE)
+    time = subprocess.Popen(time_command, stderr=subprocess.PIPE, stdout=subprocess.PIPE)
+    size = subprocess.Popen(size_command, stderr=subprocess.PIPE, stdout=subprocess.PIPE)
 
     #o time de execução do processo(que é o que queremos) é retornado no stderr
     #stdout retorna a execução do processo em si.(mostra o passo a passo da compactação do arquivo)
-    result = p.stderr
+    result_time = time.stderr
+    result_size = size.stdout
 
     #readlines retorna uma lista de strings, onde cada string representa uma linha de result
-    erro = result.readlines()
-
-    print erro[0]
+    time = result_time.readlines()
+    size = result_size.readlines()
 
     #lista que recebera o stderr separado por palavras
     output = []
-    #lista aux q recebe apenas a primeira linha de stderr(que é o q interessa, linha q contem, user, sys, elapsed, cpu)
-    listaux = [erro[0]]
 
     #for que separa a linha 1 de stderr por palavras, atraves dos espaços(toda vez q encontra um espaço ' ' ele define uma palavra
     #e joga para dentro da lista output, que passa a ser uma lista de strings, onde cada string representa o valor de uma das variaveis de estudo
-    for item in listaux:
+    for item in [time[0]]:
         items = item.split(' ')
         output.append(items)
 
@@ -42,15 +42,17 @@ def csv_creator(codigo1, codigo2, nome_arq, arquivo):
     system = (re.sub(r'[^\d.]+','', system))
     elapsed = (re.sub(r'[^\d.:]+','', elapsed))
     cpu = (re.sub(r'[^\d.%]+','', cpu))
+    compacted_file_size = (re.sub(r'[^\d]+', '', size[0]))
 
     print user
     print system
     print elapsed
     print cpu
+    print compacted_file_size
 
     #escreve a variavel aux em uma nova linha no um arquivo csv criado no doc 'targz.py'
     #'ab' do parametro de open refere-se a append b, b vai em todos
-    aux = [user, system, elapsed, cpu]
+    aux = [user, system, elapsed, cpu, compacted_file_size]
     with open('/home/marcella/PycharmProjects/compactador_de_arquivos/csv/rar/rar.csv', 'ab') as csvfile:
         writer = csv.writer(csvfile)
         writer.writerow(aux)
--- scripts/zip.py
+++ scripts/zip.py
@@ -16,22 +16,18 @@ dictcodigo = {
 }
 
 dictarquivo = {
-    1:'/home/marcella/PycharmProjects/compactador_de_arquivos/data/file',
-    2:'/home/marcella/PycharmProjects/compactador_de_arquivos/data/file',
-    3:'/home/marcella/PycharmProjects/compactador_de_arquivos/data/file',
-    4:'/home/marcella/PycharmProjects/compactador_de_arquivos/data/file',
-    5:'/home/marcella/PycharmProjects/compactador_de_arquivos/data/file'
+    1:'/home/marcella/PycharmProjects/compactador_de_arquivos/data/8mb_filefile',
 }
 
 #cria um arquivo csv e define seu cabeçalho
 with open('/home/marcella/PycharmProjects/compactador_de_arquivos/csv/zip/zip.csv', 'wb') as csvfile:
-    header = ['User-Time', 'System-Time', 'Elapsed-Time', 'CPU-Usage']
+    header = ['User-Time', 'System-Time', 'Elapsed-Time', 'CPU-Usage','Compacted-File-Size']
     writer = csv.DictWriter(csvfile, fieldnames=header)
     writer.writeheader()
 
 #laço que chama a função responsavel por captar os dados de cada compactação e escreve-los em uma nova linha o arquivo csv
-for i in range(len(dictarquivo)):
-    teste = get_file_csv.csv_creator(dictcodigo['codigo1'], dictcodigo['codigo2'], dictcodigo['nome_arq'], dictarquivo[i + 1])
+for i in range(50):
+    teste = get_file_csv.csv_creator(dictcodigo['codigo1'], dictcodigo['codigo2'], dictcodigo['nome_arq'], dictarquivo[1])
 
 csvfile.close()
 print teste
